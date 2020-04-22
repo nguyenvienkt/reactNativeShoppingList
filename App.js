f9e8a269e5592d809630d27191eecbf8c658bd51
Vien Nguyen <nguyenvienkt@gmail.com>
2020-04-23 00:00:30 +0700

    ok

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Chào Dude</Text>
+      <Image
+        source={{uri: 'https://randomuser.me/api/portraits/men/2.jpg'}}
+        style={styles.img}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'blue',
+    fontSize: 50,
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+});
 
 export default App;
